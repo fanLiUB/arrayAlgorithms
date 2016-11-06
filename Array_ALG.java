2c92f8e00824c087eff351c36ed307dc59a5514a
fanLiUB <kripkeli@bu.edu>
2016-11-06 13:53:37 -0500

    two more solved (total of 5)

--- Array_ALG.java
+++ Array_ALG.java
@@ -1,18 +1,21 @@
 // a few simple algorithms
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 
 class Array_ALG{
    // swap two chars in a char[]
-   public static void swap(char[] str, int i, int j){
+   private static void swap(char[] str, int i, int j){
      char temp = str[i];
      str[i] = str[j];
      str[j] = temp;
    }
-   
+
    // reverse a char[]
    // two-pointer technique
-   public static void reverse(char[] str){
+   private static void reverse(char[] str){
      int i = 0, j = str.length - 1;
      while (i < j){
        swap(str, i, j);
@@ -20,10 +23,10 @@ class Array_ALG{
        j--;
      }
    }
-   
+
    // remove duplicates from sorted array
    // can't allocate new mem space
-   public static int remove_duplicate(int[] l){
+   private static int remove_duplicate(int[] l){
      int len = l.length;
      if (len <= 1)
        return len;
@@ -34,12 +37,12 @@ class Array_ALG{
      }
      return len;
    }
-   
+
    // given a list of ints l, and a target int t
-   // find the indices of two int in l 
+   // find the indices of two int in l
    // which sum to t
    // index 1 < index 2
-   public static int[] sumTo(int[] l, int target){
+   private static int[] sumTo(int[] l, int target){
      int[] res = {-1,-1};
      if (l.length < 2)
        return res;
@@ -57,23 +60,73 @@ class Array_ALG{
      }
      return res;
    }
-   
-     public static void main (String args[]){
+
+
+   // given a list of chars l
+   // output the frequency o each char in l
+   private static void countFreq(char[] l){
+     Map<Character, Integer> m = new HashMap<Character, Integer>();
+     for (int i = 0; i < l.length; i++){
+       if (m.containsKey(l[i])){
+         m.put(l[i], m.get(l[i]) + 1);
+       }
+       else {
+         m.put(l[i], 1);
+       }
+     }
+     for (Map.Entry<Character, Integer> entry : m.entrySet()){
+       System.out.println("'" + (char)(entry.getKey()) + "' = " + entry.getValue());
+     }
+   }
+
+   // given two strings a and b
+   // determine if a is an anagram of b
+   private static Boolean anagram(String a, String b){
+     int[] la = new int[26];
+     int[] lb = new int[26];
+
+     for (int i = 0; i < a.length(); i++){
+       la[a.charAt(i) - 97] += 1;
+     }
+
+     for (int j = 0; j < b.length(); j++){
+       lb[b.charAt(j) - 97] += 1;
+     }
+
+     System.out.println("la = " + Arrays.toString(la));
+     System.out.println("lb = " + Arrays.toString(lb));
+
+     return Arrays.equals(la, lb);
+    }
+
+   public static void main (String args[]){
 //   char[] str1 = { 'a', 'b', 'c', 'd', 'e' };
 //   char[] str2 = { 'a', 'b', 'c', 'd', 'e', 'f' };
 //   reverse(str1);
 //   reverse(str2);
 //   System.out.println(str1);
 //   System.out.println(str2);
-    
+
 //    int[] l = {2,4,5,6,6,7,8,8,9,12,22,23,23};
 //    System.out.println(Arrays.toString(l));
 //    int len = remove_duplicate(l);
 //    System.out.println("\n" + l.length + "\n" + len);
 //    System.out.println(Arrays.toString(l));
-       
-     int[] l = {2,2,5,6,7,8,8,12,12,13,23,45,46};
-     int[] r = sumTo(l, 30);
-     System.out.println(Arrays.toString(r));
+
+//     int[] l = {2,2,5,6,7,8,8,12,12,13,23,45,46};
+//     int[] r = sumTo(l, 30);
+//     System.out.println(Arrays.toString(r));
+//     char[] l = "raeshew3WRHTSYARTR".toCharArray();
+//     countFreq(l);
+
+//     String s1 = "abcdefg";
+//     String s2 = "acbgfde";
+//     System.out.println(anagram(s1, s2));
+//     s1 = "car";
+//     s2 = "rat";
+//     System.out.println(anagram(s1, s2));
+//     s1 = "iamlordvoldemort";
+//     s2 = "tommarvoloriddle";
+//     System.out.println(anagram(s1, s2));
   }
 }
--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-// a bunch of array-problem algorithms
+# a bunch of array-problem algorithms
 
-- reverse array elements
-- length of array after removing duplicates
-- indices of two array elements which sum to a target integer
+## Problems
+
+1. reverse list elements
+
+2. remove duplicates and return new length
+
+3. given list l, target t, return indices of elements in l which sum to t
+
+4. Problem 4: given a char[] l, count the frequency of each element in l using hashMap
+
+5. Problem 5: given two strings s1 and s2, check if s2 is the anagram of s2
